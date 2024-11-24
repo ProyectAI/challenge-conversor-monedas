1507eabdd6140624ad73c42ca92876118e400d0c
cesar <AT71680124@idat.edu.pe>
2024-11-23 20:04:33 -0500

    actualizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner p = new Scanner(System.in);
+        System.out.println("-----------------Elige la moneda de conversión-----------------");
+        String dato = p.nextLine();
+
+        String direcc = "https://v6.exchangerate-api.com/v6/a94369d46416e52d5cdab07a/latest/"+dato ;
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request= HttpRequest.newBuilder()
+                .uri(URI.create(direcc))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+        
+
     }
+
+
+
+
 }
